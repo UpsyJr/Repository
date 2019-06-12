54e4dd02399ecdaeb2485ef476dd18061c50260c
UpsyJr <47011269+UpsyJr@users.noreply.github.com>
2019-06-12 14:22:54 +0300

    Update modulis.py

--- modulis.py
+++ modulis.py
@@ -4,6 +4,9 @@
 import time
 # Import the ADS1x15 module.
 import Adafruit_ADS1x15
+
+import board
+import digitalio
 import adafruit_character_lcd.character_lcd as character_lcd
 import sys
 import RPi.GPIO as GPIO
